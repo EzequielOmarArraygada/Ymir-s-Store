8fb332ac362c5ad8794da9a166c3fbd5ed882286
Ezequiel Arraygada <arraygadaezequiel@gmail.com>
2024-10-04 09:37:23 -0300

    404

--- src/app.js
+++ src/app.js
@@ -103,9 +103,7 @@ app.get('/failregister', (req, res) => {
 app.use(router);
 
 app.use((req, res, next) => {
-    res.status(404).json({
-        error: 'Ruta no encontrada',
-    });
+    res.render('error404')
 });
 
 
